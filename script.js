b2cf1b9e87a8360b124ab07981320d02bf449b85
unknown <bhumibhardwaj267@gmail.com>
2025-10-02 18:13:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('Search-button');
+const input = document.getElementById('city-input');
+const cityName = document.getElementById('city-name');
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
+
+
+async function getData(cityName){
+    const promise= await fetch(`http://api.weatherapi.com/v1/current.json?key=e665ed665f404819954102446250210&q=${cityName}&aqi=yes`);
+ return promise.json();
+}
+
+
+button.addEventListener('click', async () => {
+    const value = input.value;
+    const result = await getData(value);
+    cityName.innerText = `${result.location.name}, ${result.location.region} ${result.location.country}`;
+    cityTime.innerText =  result.location.localtime;
+    cityTemp.innerText =  result.current.temp_c;
+
+
+});
+
+//http://api.weatherapi.com/v1/current.json?key=e665ed665f404819954102446250210&q=London&aqi=yes
